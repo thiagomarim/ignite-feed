956ae0130bf131e7a34b8cea4bcbf90e2ef19832
Thiago <thiago.marim2005@gmail.com>
2024-01-18 19:10:18 -0300

    Sidebar criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from "react";
 import "./global.css";
 import { Header } from "./components/Header";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <div>
+      <aside className={styles.sidebar}>
+        <img
+          src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          className={styles.cover}
+        />
+        <div className={styles.profile}>
+          <img
+            src="https://avatars.githubusercontent.com/u/137715251?v=4"
+            alt=""
+            className={styles.avatar}
+          />
+
+          <strong>Thiago Marim</strong>
+          <span>Web Developer</span>
+        </div>
+
+        <footer>
+          <a href="#">
+            <PencilLine size={"20px"} />
+            Editar seu perfil
+          </a>
+        </footer>
+      </aside>
+    </div>
+  );
+}
